4dfa31ec87f1f2320b3d016418c34cbc11903eec
Graham <giaddon@gmail.com>
2020-07-13 10:33:28 -0700

    Beginning of snake class and movement functions

--- script.js
+++ script.js
@@ -1,12 +1,65 @@
 const DIMENSIONS = {x: 10, y: 10};
 let gameboard = [];
-let snake = {
-  length: 2,
-  x: 3,
-  y: 3,
+
+class Snake {
+  constructor(length=1, head=new Cell(3,3)) {
+    this.length = length;
+    this.head = head;
+    this.tail = head;
+    this.maxlength = 2;
+    this.body=[this.head];
+  }
+
+  slither(direction) {
+    let newTailPos = {x: this.tail.x, y: this.tail.y}
+
+    switch (direction) {
+      case "down":
+        for (let cell of this.body) {
+          cell.y += 1;
+        }
+        break;
+    
+      case "up":
+        for (let cell of this.body) {
+          cell.y -= 1;
+        }
+        break;
+      
+
+      case "left":
+        for (let cell of this.body) {
+          cell.x -= 1;
+        }
+        break;
+
+      case "right":
+        for (let cell of this.body) {
+          cell.x += 1;
+        }
+        break;
+    }
+    if (this.length < this.maxlength) {
+      let newTail = new Cell(newTailPos.x, newTailPos.y);
+      this.body.push(newTail);
+      this.tail = newTail;
+      this.length += 1;
+    }
+  
+  }
+
 }
 
-window.onload = start;
+class Cell {
+  constructor(x=0,y=0) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+let snake = new Snake();
+
+
 
 function createBoard() {
   for (let y = 0; y < DIMENSIONS.y; y += 1) {
@@ -37,25 +90,28 @@ function drawBoard() {
 
 function cycle() {
   function drawSnake() {
-    const targetCell = document.getElementById(`${snake.y}-${snake.x}`);
-    targetCell.style.setProperty("background-color", "tomato"); 
+    const visualBoard = document.querySelector("#gameboard");
+    const boardCells = visualBoard.getElementsByClassName("cell");
+    for (let cell of boardCells) {
+      cell.style.setProperty("background-color", "bisque");
+    }
+
+    for (let cell of snake.body) {
+      const targetCell = document.getElementById(`${cell.y}-${cell.x}`);
+      targetCell.style.setProperty("background-color", "tomato"); 
+    }
   }
 
   drawSnake();
 }
 
-function highlightCell(y,x) {
-  const targetCell = document.getElementById(`${y}-${x}`);
-  targetCell.style.setProperty("background-color", "tomato");
-}
-
 function start() {
   document.addEventListener('keydown', (event) => {
     const keyName = event.key;
-    if (keyName === 'ArrowDown') snake.y += 1;
-    if (keyName === 'ArrowUp') snake.y -= 1;
-    if (keyName === 'ArrowLeft') snake.x -= 1;
-    if (keyName === 'ArrowRight') snake.x += 1;
+    if (keyName === 'ArrowDown') snake.slither("down");
+    if (keyName === 'ArrowUp') snake.slither("up");
+    if (keyName === 'ArrowLeft') snake.slither("left");
+    if (keyName === 'ArrowRight') snake.slither("right");
 
     cycle();
   });
@@ -64,6 +120,9 @@ function start() {
   cycle();
 }
 
+
+
+window.onload = start;
 
 
   
